fe50f0380af2f11ceefb1bc8f109d30ef9c78e11
Om <ombhoge1234@gmail.com>
2023-02-21 21:43:12 +0530

    Commit

--- Assignment 2/FileServerClient/MyServer.java
+++ Assignment 2/FileServerClient/MyServer.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-public class MyServer {
+public class MyServer{
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(6666);
